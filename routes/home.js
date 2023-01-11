733f1e828a82781a052517ab11f8b39c4126946d
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-01-11 20:38:09 -0300

    primeiro commit

---
+++ routes/home.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "bielzin",
+    message: "Bielzin gostozin",
+  });
+});
+
+module.exports = router;
